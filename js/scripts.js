b8d04df8482c5bc1c8b802f022d66dc8c353d92f
ThiraTheNerd <thiragithinji@gmail.com>
2021-03-08 18:05:30 +0300

    define the functions in the js file

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+let total = 0;
+
+function Pizza(type, size, crust, topping, price){
+  this.type = type;
+  this.size = size;
+  this.crust = crust;
+  this.topping = topping;
+  this.price = price;
+}
