68d890850c1bc7bcfa39dc748fccd93b9f1f6a1b
ThiraTheNerd <thiragithinji@gmail.com>
2021-03-08 19:32:06 +0300

    add changes made to the html

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 let total = 0;
 
-function Pizza(type, size, crust, topping, price){
+function Pizza(type, size, crust, topping, total){
   this.type = type;
   this.size = size;
   this.crust = crust;
   this.topping = topping;
-  this.price = price;
+  this.total = total;
 }
 
 $(document).ready(function(){
@@ -23,7 +23,30 @@ $(document).ready(function(){
     console.log(selectedTopping.join(" , "));
 
     switch(pizzaSize){
-      case "0": price = 0
+      case "0": price = 0;
+      break;
+      case "large": price = 1200;
+      console.log(price);
+      break;
+      case "medium": price = 900;
+      console.log(price);
+      break;
+      case "small": price = 600;
+      console.log(price);
+      default: console.log("error")
+    }
+    switch(pizzaCrust){
+      case "0": crustPrice = 0;
+      break;
+      case "crispy": crustPrice = 100;
+      console.log(crustPrice);
+      break;
+      case "stuffed": crustPrice = 200;
+      console.log(crustPrice);
+      break;
+      case "gluten-free": crustPrice = 300;
+      console.log(crustPrice);
+      default: console.log("error")
     }
   });
 });
