0e5f4310e8cd39b40ef5d89e370dab4a26433c56
ThiraTheNerd <thiragithinji@gmail.com>
2021-03-08 19:31:36 +0300

    add the function to capture pizza size prices

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,20 @@ function Pizza(type, size, crust, topping, price){
 
 $(document).ready(function(){
   $("button#proceed-btn").click(function(event){
-    alert("Proceed button has been clicked")
+    event.preventDefault();
+
+    var pizzaName = $("#type option:selected").val();
+    var pizzaSize = $("#size option:selected").val();
+    var crustType = $("#crust option:selected").val();
+    var selectedTopping = [];
+    $.each($("input[name='toppings']:checked"), function(){
+      selectedTopping.push($(this).val());
+      
+    });
+    console.log(selectedTopping.join(" , "));
+
+    switch(pizzaSize){
+      case "0": price = 0
+    }
   });
 });
