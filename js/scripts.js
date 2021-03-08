2624633b448882ac340e14372b3a4ff715b5e411
ThiraTheNerd <thiragithinji@gmail.com>
2021-03-08 18:12:09 +0300

    add the links to link the js file

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,9 @@ function Pizza(type, size, crust, topping, price){
   this.topping = topping;
   this.price = price;
 }
+
+$(document).ready(function(){
+  $("button#proceed-btn").click(function(event){
+    alert("Proceed button has been clicked")
+  });
+});
