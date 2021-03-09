954a2d8d7222c15fe01e42daf336adaa6f47ac3e
ThiraTheNerd <thiragithinji@gmail.com>
2021-03-09 10:05:27 +0300

    add the functionality of the add button

--- js/scripts.js
+++ js/scripts.js
@@ -62,5 +62,49 @@ $(document).ready(function(){
     $("#pizza-size").html($("#size option:selected").val());
     $("#pizza-price").html(total);
 
+    // Add pizza button
+    $("button#addPizza").click(function(){
+      let pizzaName = $(".name option:selected").val();
+      let pizzasize = $("#size option:selected").val();
+      let crustType = $("#crust option:selected").val();
+      let selectedTopping = [];
+      $.each($("input[name='toppings']:checked"), function(){            
+          ptopping.push($(this).val());
+      });
+      console.log(ptopping.join(", "));
+      switch(psize){
+        case "0":
+          price =0;
+        break;
+        case "large":
+           price = 1200;
+           console.log(price);
+         break;
+         case "medium":
+           price = 850;
+           console.log("The price is "+price);
+         break;
+         case "small":
+           price = 600;
+           console.log(price);
+         default:
+           console.log("error"); 
+       }
+       switch(pcrust){
+          case "0":
+            crust_price = 0;
+          break;
+          case "Crispy":
+            crust_price = 200;
+          break;
+          case "Stuffed":
+            crust_price = 150;
+          break;
+          case "Gluten-free":
+            crust_price = 180;
+          break;
+          default:
+            console.log("No price"); 
+        }
   });
 });
